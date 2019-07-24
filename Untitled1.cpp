8285194514b921be58faf7b0ddd931c8872bb545
RizeN98 <53214723+RizeN98@users.noreply.github.com>
2019-07-24 11:17:13 +0700

    Delete 5.png

---
+++ Untitled1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int x = 10;
+	float y;
+	double z;
+	char c;
+	
+	// y = 3.14;
+	
+	// z = 4.44 + 2;
+	// z = y + 3.33;
+	scanf("%d",&x);
+	scanf("%f",&y);
+	scanf("%lf",&z);
+	
+	printf("Tuoi %d la tuoi tre \n",x);
+	printf("Gia tri cua z = %lf\n",z);
+	printf("gia tri cua y = %f\n",y);
+	
+}
+	
